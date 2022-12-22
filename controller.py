b2cd59b7e3a30642cc871eed1bff9e58ab9368e2
CarlosWS25 <carloscrodrigues2011@gmail.com>
2022-12-22 02:08:06 +0000

    updates 1
    Added new functions/libraries as:
    - prettytable()
    - os
    - time
    
    Created functions:
    - tabela() (not working atm)
    - registar_jogadores()
    - menu()
    
    Added restrictions for the begining of the program.
    
    Co-Authored-By: JotaK14 <121192279+JotaK14@users.noreply.github.com>
    Co-Authored-By: StuGu04 <117283524+StuGu04@users.noreply.github.com>

---
+++ controller.py
@@ -0,0 +1,35 @@
+import os
+import time
+from prettytable import PrettyTable
+
+def tabela(assuntos, nome, jogos, vitorias, lista_players):
+    for i in range(len(nome)): 
+        assuntos.add_row([lista_players[i]["Nome"], jogos[i], vitorias[i]])
+    return assuntos
+
+def registar_jogadores(comando, jogos, vitorias, nome):
+    nome.append(comando[1])
+    jogos.append(0)
+    vitorias.append(0)
+def menu():
+    print(
+"""
+╔═══════════════════════════════════════════════════╗
+║         Insira um dos seguintes comandos:         ║
+╠═══════════════════════════════════════════════════╣
+║            [RJ Nome] - Registar Jogador           ║
+║            [EJ Nome] - Eliminar Jogador           ║ 
+║              [LJ] - Listar Jogadores              ║
+║                [IJ] - Iniciar Jogo                ║
+║              [DJ] - Detalhes do Jogo              ║
+║                [D Nome]- Desistir                 ║
+║   CP TamanhoPeça Posição Sentido] - Colocar Peça  ║
+║            [V] - Visualizar Resultado             ║
+║             [G NomeFicheiro] - Gravar             ║
+║              [L NomeFicheiro] - Ler               ║                    
+╚═══════════════════════════════════════════════════╝
+""")
+    
+
+
+
