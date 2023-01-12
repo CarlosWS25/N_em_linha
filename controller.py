8545c06407fe40a74ae4d32f4bd53665076612d7
CarlosWS25 <carloscrodrigues2011@gmail.com>
2023-01-12 20:27:48 +0000

    Some fixes
    - detetor_iguais() almost fixed
    -  updated tabela()
    - added eliminar_jogador()

--- controller.py
+++ controller.py
@@ -2,22 +2,13 @@ import os
 import time
 from prettytable import PrettyTable
 
-def tabela(assuntos : PrettyTable, nome : list, lista_players : list):
-    for i in range(len(lista_players[0]["Nome"])): 
-        assuntos.add_row([lista_players[0]["Nome"][i], lista_players[1]["Jogos"][i], lista_players[2]["Vitórias"][i]])
+def tabela(assuntos : PrettyTable, nome , jogos, vitorias : list):
+    for i in range(len(nome)): 
+        assuntos.add_row(nome[i], jogos[i], vitorias[i])
         return assuntos
 
-def registar_jogadores(comando, jogos : list, vitorias : list, nome : list):
-    if len(comando) != 2:
-        os.system("cls")
-        menu()
-        comando = input("Insira um comando: ").split(" ")
-    else:    
-        nome.append(comando[1])
-        jogos.append(0)
-        vitorias.append(0)
 
-def registar_jogadores(comando, jogos, vitorias, nome):
+def registar_jogadores(comando: list, jogos: list, vitorias: list, nome: list):
     nome.append(comando[1])
     jogos.append(0)
     vitorias.append(0)
@@ -46,10 +37,18 @@ def apagar(i):
     time.sleep(i)
     os.system("cls")
 
-def detetor_iguais(lista_players, comando):
-    for i in range(len(lista_players[0]["Nome"])):
-        if lista_players[0]["Nome"][i] == comando[1]:
+def detetor_iguais(nome: list, comando: list):
+    for i in range(len(nome)):
+        if nome[i] == comando[1]:
             igual = True
         else:
             igual = False
-    return igual
+
+def eliminar_jogador(i, nome: list, comando: list):
+    for i in range (len(nome[i])):
+        if nome[i] == comando[1]:
+            del nome[i]
+
+
+
+
--- view.py
+++ view.py
@@ -33,71 +33,101 @@ def main():
                 if comando[0].lower() == "ej":
                     os.system("cls")
                     print("Não pode eliminar jogadores sem antes os registar...")
-                    apagar(2) 
+                    apagar(2)
+                    break
                 elif comando[0].lower() == "lj":
                     os.system("cls")
                     print("Não pode listar jogadores sem antes os registar...")
-                    apagar(2) 
+                    apagar(2)
+                    break
                 elif comando[0].lower() == "ij":
                     os.system("cls")
                     print("Não pode iniciar jogadores enquanto não tiver jogadores registados...")
                     apagar(2)  
+                    break
                 elif comando[0].lower() == "dj":
                     os.system("cls")
                     print("Não pode listar jogadores sem antes os registar...")
-                    apagar(2)   
+                    apagar(2)
+                    break 
                 elif comando[0].lower() == "d":
                     os.system("cls")
                     print("Não pode desistir ter começado o jogo (não tem jogadores registados)")
-                    apagar(2)   
+                    apagar(2)
+                    break   
                 elif comando[0].lower() == "cp":
                     os.system("cls")
                     print("Não pode colocar peça sem ter começado o jogo (não tem jogadores registados)")
-                    apagar(2)  
+                    apagar(2)
+                    break  
                 elif comando[0].lower() == "v":
                     os.system("cls")
                     print("Não pode visualizar resultado sem ter jogadores registados...")
                     apagar(2)
+                    break
                 elif comando[0].lower() == "dj":
                     os.system("cls")
                     print("Não pode ver detalhes de jogo sem antes ter iniciado jogo...")
                     apagar(2)
+                    break
                 elif comando[0].lower() == "dj":
                     os.system("cls")
                     print("Não existe progresso para ser guardado...")
-                    apagar(2)    
+                    apagar(2)
+                    break    
                 else:
-                    os.system("cls")       
+                    os.system("cls")
+                    print("Instrução inválida.")
+                    apagar(2)     
+                    break  
             if comando[0].lower() == "rj":
                 if len(comando) != 2:
-                        os.system("cls")
+                    os.system("cls")
                 else:
                     os.system("cls")
                     registar_jogadores(comando, jogos, vitorias, nome)
+                    print(lista_players)
                     print("Jogador registado com sucesso.")
                     apagar(2)
             elif comando[0].lower() == "l":
                 #carregar ficheiro load (Ainda não aprendemos)
                 pass
         else:
-            if comando[0].lower() == "rj":
-                if len(comando) != 2:
+            while comando[0].lower() != "ij":
+                if comando[0].lower() == "rj":
+                    if len(comando) != 2:
                         os.system("cls")
-                else:
-                    verif = detetor_iguais(lista_players, comando)
-                    if verif == True:
-                       os.system("cls")
-                       print("Já existe um jogador com esse nome...")
-                       apagar(2)
+                        break
                     else:
+                        verif = detetor_iguais(lista_players, comando)
+                        if verif == True:
+                            os.system("cls")
+                            print("Já existe um jogador com esse nome...")                            
+                            print(lista_players)
+                            apagar(2)
+                            break
+                        else:
+                            os.system("cls")
+                            registar_jogadores(comando, jogos, vitorias, nome)
+                            print("Jogador registado com sucesso.")
+                            print(lista_players)
+                            apagar(2)
+                            break
+                elif comando[0].lower() == "lj":
+                    os.system("cls")
+                    table = tabela(assuntos, nome, jogos, vitorias)
+                    print(table)
+                    apagar(2)
+                    break
+                elif comando[0].lower() == "ej":
+                    while comando[1] not in lista_players[0]["Nome"]:
                         os.system("cls")
-                        registar_jogadores(comando, jogos, vitorias, nome)
-                        print("Jogador registado com sucesso.")
+                        print("Jogador não existente (Sistema sensível a maísculas e minúsculas.")
                         apagar(2)
-            elif comando[0].lower() == "lj":
-                table = tabela(assuntos, nome, lista_players)
-                print(table)
-        
+                        break
+                    
+
+            
 
 
 
