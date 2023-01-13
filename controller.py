90274277637e773ee71a479476e11255e6ccc356
CarlosWS25 <carloscrodrigues2011@gmail.com>
2023-01-13 23:52:53 +0000

    Added more functions and changed some stuff
    + carregar_jogo (not working atm)
    + gravar_jogo (not working atm)
    + cair_peca() (not working atm)
    + mesa_jogo()
    + bubblesort()
    + criar_list_ingame()
    + some alterations done in view.py
    
    Co-Authored-By: JotaK14 <121192279+JotaK14@users.noreply.github.com>
    Co-Authored-By: StuGu04 <117283524+StuGu04@users.noreply.github.com>

--- controller.py
+++ controller.py
@@ -1,26 +1,34 @@
 import os
 import time
+import json
+import numpy as np
 from prettytable import PrettyTable
+
+
 
 def tabela(assuntos : PrettyTable, nome: list , jogos: list, vitorias: list):
     assuntos.clear_rows()
     for i in range(len(nome)): 
         assuntos.add_row([nome[i], jogos[i], vitorias[i]])
-    return assuntos
-
+    return assuntos             
 
 def registar_jogadores(comando: list, jogos: list, vitorias: list, nome: list):
     nome.append(comando[1])
     jogos.append(0)
     vitorias.append(0)
-    
-    
+        
 def apagar(i):
     time.sleep(i)
     os.system("cls")
 
 def detetor_iguais(nome: list, comando: list):
     if comando[1] in nome:
+        return True
+    else:
+        return False
+
+def detetor_players(nome: list, comando: list):
+    if comando[1] and comando[2] in nome:
         return True
     else:
         return False
@@ -32,6 +40,37 @@ def eliminar_jogador(nome: list, comando: list):
     else:
         return False
 
+def carregar_jogo(save):
+    with open(save) as s:
+        info = json.load(s)
+    return info
 
+def gravar_jogo(save, l):
+    json_string = json.dumps(l)
+    json_file = open(save, "w")
+    json_file.write(json_string)
+    json_file.close()
+
+def mesa_jogo(comando_int : list):
+    return np.full((comando_int[0], comando_int[1]), "_")
+
+def bubblesort(nome):
+    n = len(nome)
+    for i in range(n):
+        for j in range(0, n-i-1):
+            if nome[j] > nome[j+1]:
+                nome[j], nome[j+1] = nome[j+1], nome[j]
+    return nome
+
+def criar_lista_ingame(lista_in_game: list, bubblesort, comando: list):
+    lista_in_game.clear()
+    lista_in_game.append(comando[1])
+    lista_in_game.append(comando[2])
+    bubblesort(lista_in_game)
+
+def cair_peca(tabuleiro, comando_int : list):
+    for k in range(comando_int[1]-1, -1, -1):
+        if tabuleiro([comando_int[0],k]) == "_":
+            tabuleiro[comando_int[0],k] = "X"
 
 
--- view.py
+++ view.py
@@ -1,14 +1,17 @@
 from controller import *
-
-#Variaveis
-nome = []
-jogos = []
-vitorias = []
-assuntos = PrettyTable()
-assuntos.field_names = ["Nome", "Jogos", "Vitórias"]
-lista_players = [{"Nome" : nome} , {"Jogos" : jogos} , {"Vitórias" : vitorias}]
-#main
 def main():
+    
+    #Variaveis
+    nome = []
+    jogos = []
+    vitorias = []
+    assuntos = PrettyTable()
+    assuntos.field_names = ["Nome", "Jogos", "Vitórias"]
+    lista_players = [{"Nome" : nome} , {"Jogos" : jogos} , {"Vitórias" : vitorias}]
+    lista_in_game = []  
+    game_start = False
+
+    #main
     os.system("cls")
     print(
 """
@@ -37,15 +40,15 @@ def main():
 ║                [IJ] - Iniciar Jogo                ║
 ║              [DJ] - Detalhes do Jogo              ║
 ║                [D Nome]- Desistir                 ║
-║  [CP TamanhoPeça Posição Sentido] - Colocar Peça  ║
+║          [CP Nome Posição] - Colocar Peça         ║
 ║            [V] - Visualizar Resultado             ║
 ║             [G NomeFicheiro] - Gravar             ║
-║              [L NomeFicheiro] - Ler               ║                    
+║              [L NomeFicheiro] - Ler               ║
 ╚═══════════════════════════════════════════════════╝
 """)
         comando = input("Insira um comando: ").split(" ")
         if lista_players[0]["Nome"] == []: 
-            while comando[0].lower() != "rj"  and comando[0].lower() != "l" :
+            while comando[0].lower() != "rj" and comando[0].lower() != "l" :
                 if comando[0].lower() == "ej":
                     os.system("cls")
                     print("Não pode eliminar jogadores sem antes os registar...")
@@ -109,16 +112,17 @@ def main():
                 #carregar ficheiro load (Ainda não aprendemos)
                 pass
         else:
-            while comando[0].lower() != "ij":
-                if comando[0].lower() == "rj":
-                    if len(comando) != 2:
-                        os.system("cls")
-                        break
-                    else:
-                        verif = detetor_iguais(nome, comando)
+            if game_start == True:
+                while True:
+                    if comando[0].lower() == "rj":
+                        if len(comando) != 2:
+                            os.system("cls")
+                            break
+                        else:
+                            verif = detetor_iguais(nome, comando)
                         if verif == True:
                             os.system("cls")
-                            print("Já existe um jogador com esse nome...")                           
+                            print("Já existe um jogador com esse nome...")                            
                             apagar(2)
                             break
                         else:
@@ -127,32 +131,190 @@ def main():
                             print("Jogador registado com sucesso.")
                             apagar(2)
                             break
-                elif comando[0].lower() == "lj":
-                    os.system("cls")
-                    table = tabela(assuntos, nome, jogos, vitorias)
-                    print(table)
-                    apagar(2)
-                    break
-                elif comando[0].lower() == "ej":
-                    if len(comando) != 2:
-                        os.system("cls")
-                        break
-                    else:
-                        apagar_player = eliminar_jogador(nome, comando)
-                        if apagar_player == True:
+                    elif comando[0].lower() == "ej":
+                        if len(comando) != 2:
                             os.system("cls")
-                            print("Jogador eliminado com sucesso!")
-                            apagar(2)
                             break
                         else:
+                            apagar_player = eliminar_jogador(nome, comando)
+                            if apagar_player == True:
+                                os.system("cls")
+                                print("Jogador eliminado com sucesso!")
+                                apagar(2)
+                                break
+                            else:
+                                os.system("cls")
+                                print("Jogador não existe!")
+                                apagar(2)
+                                break
+                    elif comando[0].lower() == "lj":
+                        os.system("cls")
+                        table = tabela(assuntos, nome, jogos, vitorias)
+                        print(table)
+                        apagar(2)
+                        break        
+                    elif comando[0].lower() == "ij":
+                        os.system("cls")
+                        print("Já existe um jogo a decorrer!")
+                        apagar(2)
+                        break
+                    elif comando[0].lower() == "dj":
+                        print(
+f"""
+Nome dos jogadores: {lista_in_game[0]} e {lista_in_game[1]}
+Comprimento: {comando_int[0]}
+Altura: {comando_int[1]}
+Quantidade de peças necessárias para vencer: {comando_int[2]}                      
+""")
+                        break
+                    elif comando[0].lower() == "cp":
+                        if len(comando) != 3:
                             os.system("cls")
-                            print("Jogador não existe!")
+                            print("Número de parametros inválido!")
                             apagar(2)
+                        elif comando[1] not in lista_in_game:
+                            os.system("cls")
+                            print("Esse jogador não está a em jogo neste momento")
+                            apagar(2)
+                        elif comando[1] not in nome:
+                            os.system("cls")
+                            print("Esse jogador não existe...")
+                            apagar(2)
+                        elif not 0 < comando_int[2] <= comando_int[0]:
+                            os.system("cls")
+                            print(f"A jogada deve ser feita dentro dos limites do comprimento do tabuleiro (0-{comando_int[0]}.")
+                            apagar(2)
+                        else:
+                            cair_peca(tabuleiro, comando_int)
+                            print(tabuleiro)
+
+                    elif comando[0].lower() == "v":
+                        os.system("cls")
+                        print(tabuleiro)
+                        apagar(2)                        
+                        break    
+            else:                       
+                while comando[0].lower() != "ij":
+                    if comando[0].lower() == "rj":
+                        if len(comando) != 2:
+                            os.system("cls")
                             break
-                                
-
-
-
+                        else:
+                            verif = detetor_iguais(nome, comando)
+                            if verif == True:
+                                os.system("cls")
+                                print("Já existe um jogador com esse nome...")                           
+                                apagar(2)
+                                break
+                            else:
+                                os.system("cls")
+                                registar_jogadores(comando, jogos, vitorias, nome)
+                                print("Jogador registado com sucesso.")
+                                apagar(2)
+                                break
+                    elif comando[0].lower() == "lj":
+                        os.system("cls")
+                        table = tabela(assuntos, nome, jogos, vitorias)
+                        print(table)
+                        apagar(2)
+                        break
+                    elif comando[0].lower() == "ej":
+                        if len(comando) != 2:
+                            os.system("cls")
+                            break
+                        else:
+                            apagar_player = eliminar_jogador(nome, comando)
+                            if apagar_player == True:
+                                os.system("cls")
+                                print("Jogador eliminado com sucesso!")
+                                apagar(2)
+                                break
+                            else:
+                                os.system("cls")
+                                print("Jogador não existe!")
+                                apagar(2)
+                                break
+                    elif comando[0].lower() == "dj":
+                        os.system("cls")
+                        print("Não pode ver os detalhes do jogo sem antes o iniciar")
+                        apagar(2)
+                        break 
+                    elif comando[0].lower() == "d":
+                        os.system("cls")
+                        print("Não pode desistir ter começado o jogo.")
+                        apagar(2)
+                        break                   
+                    elif comando[0].lower() == "cp":
+                        os.system("cls")
+                        print("Não pode colocar peça sem ter começado o jogo.")
+                        apagar(2)
+                        break          
+                    elif comando[0].lower() == "v":
+                        os.system("cls")
+                        print("Não existe jogo em curso.")
+                        apagar(2)
+                        break        
+                    elif comando[0].lower() == "g":
+                        pass
+                        #fazer no fim
+                    elif comando[0].lower() == "l":
+                        pass
+                        #carregar ficheiro load (Ainda não aprendemos)
+                if comando[0].lower() == "ij":
+                    if game_start == True:
+                        os.system("cls")
+                        print("Já existe um jogo a decorrer!")
+                        apagar(2)
+                    elif len(comando) != 3:
+                        os.system("cls")
+                        print("Apenas podem jogar 2 jogadores!")
+                        apagar(2)
+                    else:
+                        verif_players = detetor_players(nome, comando)
+                        if verif_players == True:
+                            os.system("cls")
+                            criar_lista_ingame(lista_in_game, bubblesort, comando)
+                            while True:
+                                comando = input(
+    """
+    ╔═══════════════════════════════════════════════════════╗
+    ║   Insira os seguintes dados pela ordem apresentada:   ║
+    ╠═══════════════════════════════════════════════════════╣
+    ║ Comprimento (espaço) Altura (espaço) TamanhoSequência ║                        
+    ╚═══════════════════════════════════════════════════════╝
+    Comando: """).split(" ")
+                                comando_int = list(map(int, comando))
+                                if len(comando_int) != 3:
+                                    os.system("cls")
+                                    print("Número de parametros inválido!")
+                                    apagar(2)
+                                elif comando_int[0] < 0:
+                                    os.system("cls")
+                                    print("Comprimento da grelha tem de ser maior do que 0!")
+                                    apagar(2)
+                                elif not (comando_int[0] / 2) <= comando_int[1] <= comando_int[0]:
+                                    os.system("cls")
+                                    print(f"A altura da grelha deve estar compreendida entre {round(comando_int[0]/2)} e {comando_int[0]}.")
+                                    apagar(2)
+                                elif not 0 < comando_int[2] <= comando_int[0]:
+                                    os.system("cls")
+                                    print(f"O número de peças em linha para determinar a vitória tem de estar compreendido entre 0 e {comando_int[0]}.")
+                                    apagar(2)
+                                else:
+                                    game_start = True
+                                    bubblesort(nome)
+                                    os.system("cls")                                
+                                    tabuleiro = mesa_jogo(comando_int)
+                                    print(f"Jogo iniciado entre {lista_in_game[0]} e {lista_in_game[1]}.")
+                                    apagar(3)
+                                    break           
+                        else:
+                            os.system("cls")
+                            print("Os jogadores em questão não se encontram registados. (Sistema sensivel a maisculas)")
+                            print(f"Lista de jogadores registados: {nome}")
+                            apagar(3)
+                    
+                    
 
 
 
